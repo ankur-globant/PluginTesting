2b5ffb837d09ade29c7e66b2a93614d149064d68
ankur-globant <ankur.upadhyay@globant.com>
2024-12-18 18:47:11 +0530

    Initial commit

---
+++ app/src/main/java/com/example/plugintesting/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.plugintesting
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mytestui.MySampleText
+import com.example.plugintesting.ui.theme.PluginTestingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PluginTestingTheme {
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                    MySampleText()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
